f4a40f95b5f12d29eb20594b508384830f825ec8
Alain Marquez <alamar1990@gmail.com>
2023-09-08 10:44:05 -0400

    Update favicon

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,13 +2,11 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
+        <q-img
+          class="self-center"
+          src="~assets/signature_icon.svg"
+          spinner-color="white"
+          style="height: 35px; max-width: 35px"
         />
 
         <q-toolbar-title> Signature collector</q-toolbar-title>
@@ -19,73 +17,10 @@
           v-ripple
           class="text-white"
         >
-          <q-item-section avatar>
-            <q-icon
-              name="login"
-              class="text-white  rotate-180"
-            />
-          </q-item-section>
 
-          <q-item-section>
-          </q-item-section>
-          <q-menu auto-close>
-            <q-list style="min-width: 100px">
-              <q-item
-                clickable
-                to="/login"
-              >
-                <q-item-section>Login</q-item-section>
-              </q-item>
-              <q-item
-                clickable
-              >
-                <q-item-section>Logout</q-item-section>
-              </q-item>
-            </q-list>
-          </q-menu>
         </q-item>
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      class="bg-grey-8"
-    >
-      <q-list dark>
-        <q-item-label header>
-          Let's see ...
-        </q-item-label>
-        <q-item
-          clickable
-          to="/protected"
-        >
-          <q-item-section avatar>
-            <q-icon name="lock" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Protected</q-item-label>
-            <q-item-label caption>
-              Protected route
-            </q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          to="/form"
-        >
-          <q-item-section avatar>
-            <q-icon name="feed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Form</q-item-label>
-            <q-item-label caption>
-              Parse
-            </q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -94,7 +29,8 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
+import { defineComponent } from 'vue'
+// import { symOutlinedSignature } from '@quasar/extras/material-symbols-outlined'
 export default defineComponent({
   name: 'MainLayout',
 
@@ -105,13 +41,8 @@ export default defineComponent({
   },
 
   setup () {
-    const leftDrawerOpen = ref(false)
-
     return {
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+
     }
   }
 })
